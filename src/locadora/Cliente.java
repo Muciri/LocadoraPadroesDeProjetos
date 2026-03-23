60cad3803b7709902cdb1f69e519bae955531371
Murilo Maciel Rodrigues <murilomacielbr@gmail.com>
2026-03-23 20:52:17 -0300

    Lista 1.2.4: criando um extrato com saída em formato HTML

--- src/locadora/Cliente.java
+++ src/locadora/Cliente.java
@@ -20,7 +20,7 @@ public class Cliente {
 		carrosAlugados.add(locacao);
 	}
 
-	public double getValorTotal() {
+	private double getValorTotal() {
 		double valorTotal = 0.0;
 		Iterator<Locacao> locacoes = carrosAlugados.iterator();
 
@@ -33,13 +33,13 @@ public class Cliente {
 		return valorTotal;
 	}
 
-	public int getPontosTotaisDeAlugadorFrequente() {
+	private int getPontosTotaisDeAlugadorFrequente() {
 		int pontosDeLocadorFrequente = 0;
 		Iterator<Locacao> locacoes = carrosAlugados.iterator();
 
 		while(locacoes.hasNext()) {
 			Locacao cada = locacoes.next();
-			pontosDeLocadorFrequente += cada.adicionaPontosDeLocadorFrequente();
+			pontosDeLocadorFrequente += cada.getPontosDeAlugadorFrequente();
 
 		} // while
 
@@ -69,6 +69,32 @@ public class Cliente {
 		resultado += "Voce acumulou " + getPontosTotaisDeAlugadorFrequente() +
               " pontos de locador frequente";
 
+		return resultado;
+	}
+
+	public String extratoHTML() {
+		final String fimDeLinha = System.getProperty("line.separator");
+		int sequencia = 0;
+		
+		Iterator<Locacao> locacoes = carrosAlugados.iterator();
+		String resultado = "<html><body>"+ fimDeLinha;
+		resultado = String.format("<H2>Registro de Locacoes de <EM> %s </EM></H2>", getNome()) + fimDeLinha;
+		resultado += "<table border=\"1\"><tr><th>Seq</th><th>Automóvel</th><th>Ano</th><th>Diárias</th><th>Valor</th></tr>" + fimDeLinha;
+		
+		while(locacoes.hasNext()) {
+			Locacao cada = locacoes.next();
+			// mostra valores para este Locacao
+			sequencia++;
+			resultado +=
+			String.format("<tr><th>%02d.</th><th>%s</th><th>%4d</th><th>%2d</th><th>R$ %8.2f</th></tr>" + fimDeLinha, sequencia,
+			cada.getCarro().getDescricao(),cada.getCarro().getAno(),
+			cada.getDiasAlugado(), cada.valorDeUmaLocacao() );
+		} // while
+		
+		// adiciona rodapé
+		resultado += String.format("<tfoot><tr><td colspan=\"4\">Valor Acumulado em diárias:</td><td><EM>R$ %8.2f</EM></td></tr></tfoot></table>" + fimDeLinha, getValorTotal());
+		resultado += "<P>Voce acumulou <EM>" + getPontosTotaisDeAlugadorFrequente() + " pontos </EM> de alugador frequente</p></body></html> ";
+		
 		return resultado;
 	}
 }
--- src/locadora/Locacao.java
+++ src/locadora/Locacao.java
@@ -18,39 +18,10 @@ public class Locacao {
 	}
 
 	public double valorDeUmaLocacao() {
-		double valorLocacao = 0.0;
-		
-		switch(carro.getCodigoDoPreco()) {
-			case Automovel.BASICO: // R$ 90.00 por dia
-				valorLocacao = this.getDiasAlugado() * 90.0;
-				break;
-
-			case Automovel.FAMILIA: // R$ 130.00 por dia
-				valorLocacao = this.getDiasAlugado() * 130.0;
-				break;
-
-			case Automovel.LUXO: // R$ 200.00 por dia
-				valorLocacao = this.getDiasAlugado() * 200.0;
-				
-				// Adiciona um desconto de 10% se alugar o carro por mais de 4 dias
-				if(this.getDiasAlugado() > 4) {
-					valorLocacao *= 0.9;
-				}
-				break;
-		} 
-
-		return valorLocacao;
+		return this.carro.valorDeUmaLocacao(this.getDiasAlugado());
 	}
 
-	public int adicionaPontosDeLocadorFrequente() {
-		int pontos = 1;
-		
-		// adiciona bonus para locação de um carro de luxo por pelo menos 3 dias
-		if(carro.getCodigoDoPreco() == Automovel.LUXO &&
-			this.getDiasAlugado() > 2) {
-			pontos += 2;
-		}
-		
-		return pontos;
+	public int getPontosDeAlugadorFrequente() {
+		return carro.getPontosDeAlugadorFrequente(this.getDiasAlugado());
 	}
 }
